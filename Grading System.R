76c73a5e4434defde30b82bf32956a1b6ae07c30
arlrom003 <arlrompedeglorio13@gmail.com>
2025-09-15 21:36:56 +0800

    Update Grading System.R

--- Grading System.R
+++ Grading System.R
@@ -1,12 +1,13 @@
-input = 72
-if(input >= 90 && <= 100){
-    print("Grade A")
-} else if (input >= 80 && <= 89) {
-    print("Grade B")
-} else if (input >= 70 && <= 79) {
-    print("Grade C")
-} else if (input >= 60 && <= 69) {
-    print("Grade D")
+input <- 72
+
+if (input >= 90 && input <= 100) {
+  print("Grade A")
+} else if (input >= 80 && input <= 89) {
+  print("Grade B")
+} else if (input >= 70 && input <= 79) {
+  print("Grade C")
+} else if (input >= 60 && input <= 69) {
+  print("Grade D")
 } else {
-    print("Grade F")
+  print("Grade F")
 }
