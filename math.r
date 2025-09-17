e056c546b3c07cfab73f9ef310c38344f745762f
arlrom003 <arlrompedeglorio13@gmail.com>
2025-09-18 00:41:41 +0800

    Update math.r

--- math.r
+++ math.r
@@ -1,10 +1,10 @@
 cat("1=Add, 2=Subtract, 3=Multiply, 4=Divide\n")
-choice <- as.integer(readline("Choose: "))
+choice = as.integer(readline("Choose: "))
 
-x <- as.numeric(readline("Enter first number: "))
-y <- as.numeric(readline("Enter second number: "))
+x = as.numeric(readline("Enter first number: "))
+y = as.numeric(readline("Enter second number: "))
 
-result <- switch(choice,
+result = switch(choice,
                  x + y,
                  x - y,
                  x * y,
